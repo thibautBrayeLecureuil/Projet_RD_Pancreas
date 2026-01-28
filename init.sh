f1a1c44cf7b73fbc3162e784dd6983abf4b7e3cd
Thibaut <t.brayelecureuil@gmail.com>
2026-01-28 11:12:45 +0100

    script de setup et lancement

---
+++ init.sh
@@ -0,0 +1,6 @@
+rm -rf Interface
+wget https://github.com/thibautBrayeLecureuil/Projet_RD_Pancreas/archive/refs/heads/main.zip
+unzip main
+mv Projet_RD_Pancreas-main/ Interface/
+rm main.zip
+python3 /home/pa/Interface/src/main.py
