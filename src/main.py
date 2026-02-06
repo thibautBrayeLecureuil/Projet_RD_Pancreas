6e363d7f0be74ed94971549f7ce0167a54858d9a
Thibaut <t.brayelecureuil@gmail.com>
2026-02-06 15:38:04 +0100

    tentative de correction

--- src/main.py
+++ src/main.py
@@ -3,32 +3,15 @@ import dataTreatment as dt
 
 app = Flask(__name__)
 
-# ATTENTION : Route '/' pour correspondre à Matlab
-@app.route('/', methods=['POST'])
-def home():
-    print("\n--- MESSAGE REÇU DE MATLAB ---")
+@app.route('/control', methods=['POST'])
+def control_loop():
+
     data = request.json
-    print(f"Données brutes reçues : {data}")
 
-    # Recherche intelligente des données patient
-    target_data = data
-    if isinstance(data, dict) and 'patient' in data:
-        target_data = data['patient']
-    elif isinstance(data, dict) and 'data' in data:
-        target_data = data['data']
-    
-    # Appel du module de calcul
-    dose_calculee = dt.process(target_data)
+    print("Received data:", data)
+    dataresponse = dt.process(data['patients'][0])
 
-    print(f"--- RÉPONSE ENVOYÉE : {dose_calculee} U/h ---")
-
-    # Format de réponse spécifique pour votre Matlab
-    return jsonify({
-        "status": "success",
-        "nouvelle_valeur": dose_calculee,  # <-- Matlab attend ce mot précis !
-        "unit": "U/h"
-    })
+    return jsonify(dataresponse)
 
 if __name__ == '__main__':
-    # '0.0.0.0' OBLIGATOIRE pour la connexion Wifi
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8081)
