40eccd5f353bff00fd996897dcc1ab6effdca34d
Thibaut <t.brayelecureuil@gmail.com>
2026-02-06 14:22:46 +0100

    traitement des données et commandes (ne fonctionne probablement pas)

--- src/dataTreatment.py
+++ src/dataTreatment.py
@@ -1,3 +1,49 @@
+import subprocess
+import datetime
+import json
+
+IOB_FILE = "../ressources/iob.json"
+GLUCOSE_FILE = "../ressources/glucose.json"
+PROFILE_FILE = "../ressources/profile.json"
+CLOCK_FILE = "../ressources/clock.json"
+PUMP_HISTORY_FILE = "../ressources/pumphistory.json"
+CURRENTTEMP_FILE = "../ressources/currenttemp.json"
+MEAL_FILE = "../ressources/meal.json"
 
 def process(data):
-    return
+
+    date = datetime.datetime.now()
+    dateString = date.isoformat() + "Z"
+    
+    glucose_data = {
+        "date": date,
+        "dateString": dateString,
+        "sgv": data["Gb"],
+        "direction": "Flat",
+        "noise": 1
+    }
+
+    with open(GLUCOSE_FILE, 'r') as f:
+        glucose_file = json.loads(f)
+
+    glucose_file.append(glucose_data)
+    
+    with open(GLUCOSE_FILE, "w") as f:
+        json.dump(glucose_file, f)
+
+
+    #appel de oref0
+    output = callLoop()
+
+    return output
+
+def callLoop():
+    pwd = subprocess.run(['pwd'])
+    if not pwd.contains(b'Interface/ressources'):
+        output = subprocess.run(['cd', '~/Interface/ressources/'])
+        
+    output = subprocess.run(['oref0-calculate-iob', PUMP_HISTORY_FILE,PROFILE_FILE ,CLOCK_FILE])
+    output = subprocess.run(['oref0-meal', PUMP_HISTORY_FILE, PROFILE_FILE, CLOCK_FILE, GLUCOSE_FILE])
+    output = subprocess.run(['oref0-determine-basal', IOB_FILE, CURRENTTEMP_FILE, GLUCOSE_FILE, PROFILE_FILE, MEAL_FILE])
+    
+    return output 
--- src/main.py
+++ src/main.py
@@ -7,21 +7,10 @@ app = Flask(__name__)
 def control_loop():
 
     data = request.json
-    glucose = data.get('glucose')
 
-    if glucose > 150:
-        print("Trop de Glucose")
-    else:
-        print("Pas trop de Glucose")
+    dataresponse = dt.process(data[0])
 
-    dt.process(data)
-
-
-    return jsonify({
-        "status": "success",
-        "insulin_dose": 100,
-        "unit": "pmol/min"
-    })
+    return jsonify(dataresponse)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
