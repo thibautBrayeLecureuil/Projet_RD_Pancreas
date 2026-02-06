0482bf80a0018c81890048b01c38d61445cd96d6
Thibaut <t.brayelecureuil@gmail.com>
2026-02-06 14:49:20 +0100

    tentative de correction

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import dataTreatment as dt
+import subprocess
 
 app = Flask(__name__)
 
@@ -8,6 +9,10 @@ def control_loop():
 
     data = request.json
     
+    pwd = subprocess.run(['pwd'])
+
+    print("Current directory:", pwd)
+
     print("Received data:", data)
     dataresponse = dt.process(data['patients'][0])
 
