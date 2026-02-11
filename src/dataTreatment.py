a6f733269b202b756df751e8ac79f4d749213a2d
tibo <t.brayelecureuil@gmail.com>
2026-02-11 11:24:07 +0100

    appel d'oref0

--- src/dataTreatment.py
+++ src/dataTreatment.py
@@ -1,6 +1,7 @@
 import subprocess
 import datetime
 import json
+import os
 
 IOB_FILE = "../ressources/iob.json"
 GLUCOSE_FILE = "../ressources/glucose.json"
@@ -10,14 +11,14 @@ PUMP_HISTORY_FILE = "../ressources/pumphistory.json"
 CURRENTTEMP_FILE = "../ressources/currenttemp.json"
 MEAL_FILE = "../ressources/meal.json"
 
+
 def process(data):
 
     date = datetime.datetime.now()
     dateString = date.isoformat() + "Z"
     
     glucose_data = {
-        "date": date.timestamp(),
-        "dateString": dateString,
+        "date": dateString,
         "sgv": data["Gb"],
         "direction": "Flat",
         "noise": 1
@@ -28,9 +29,13 @@ def process(data):
 
     glucose_file.append(glucose_data)
 
+    callLoop()
+
     with open(GLUCOSE_FILE, "w") as f:
         json.dump(glucose_file, f)
 
 def callLoop():
     subprocess.run(['oref0-calculate-iob', PUMP_HISTORY_FILE,PROFILE_FILE ,CLOCK_FILE])
+    subprocess.run(['oref0-meal', PUMP_HISTORY_FILE, PROFILE_FILE, CLOCK_FILE, GLUCOSE_FILE])
     subprocess.run(['oref0-determine-basal', IOB_FILE, CURRENTTEMP_FILE, GLUCOSE_FILE, PROFILE_FILE])
+    print("IOB calculation done")
