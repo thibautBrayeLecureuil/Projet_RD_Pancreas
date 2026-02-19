9587286adb6005b79b6c39bbbcc5b2079358920f
Fernandes Lucas <lucas.fernandes3319@gmail.com>
2026-02-19 15:36:07 +0100

    Commit avec nouveau fichier de basal

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ wget https://github.com/thibautBrayeLecureuil/Projet_RD_Pancreas/archive/refs/he
 unzip main
 mv Projet_RD_Pancreas-main/ Interface/
 rm main.zip
-cd Interface/src
+cd Interface
 python3 main.py
