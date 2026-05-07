5174bdd2e0e847e8e5858ee966001725d44fba18
Tibo <103729425+thibautBrayeLecureuil@users.noreply.github.com>
2026-05-07 11:45:09 +0200

    Enhance init.sh for project setup and Flask installation
    Added project setup and dependency installation steps.

--- init.sh
+++ init.sh
@@ -1,7 +1,40 @@
+echo "====================================="
+echo " --- Recupération du projet PAOA --- "
+echo "====================================="
+
 rm -rf Interface
 wget https://github.com/thibautBrayeLecureuil/Projet_RD_Pancreas/archive/refs/heads/main.zip
 unzip main
 mv Projet_RD_Pancreas-main/ Interface/
 rm main.zip
+
+echo "====================================="
+echo " - Installation Flask + dépendances -"
+echo "====================================="
+
+# Vérifie que Python3 est installé
+if ! command -v python3 &> /dev/null
+then
+    echo "Python3 n'est pas installé."
+    exit 1
+fi
+
+# Vérifie que pip est installé
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 n'est pas installé."
+    exit 1
+fi
+
+sudo apt update
+
+pip install --upgrade pip
+
+pip install flask
+
+echo "====================================="
+echo " ---- Lancement de l'interface ---- "
+echo "====================================="
+
 cd Interface
 python3 ./src/main.py
